f8b9774ef042ef324d7069954d2bf770a70f1db2
savvinov ivan <morningjacketup@gmail.com>
2022-10-12 19:59:01 +0600

    step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,11 @@
 package hexlet.code;
 
 
-import static hexlet.code.Cli.Greeting;
+import static hexlet.code.Cli.greeting;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games");
-        Greeting();
+        greeting();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    static void Greeting() {
+    static void greeting() {
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name?");
         while (sc.hasNext()) {
