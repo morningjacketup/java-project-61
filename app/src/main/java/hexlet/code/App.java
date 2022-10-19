8dba852ba39ad490600c84bab4b1a95809d3c585
washburn <washburn@DESKTOP-PQRPMOP.localdomain>
2022-10-19 21:59:35 +0600

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,6 +14,7 @@ import static hexlet.code.Cli.greeting;
 public class App {
     public static void main(String[] args) {
 
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
