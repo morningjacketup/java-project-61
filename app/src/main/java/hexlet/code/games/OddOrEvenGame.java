8b89f510b9e2e2f1dafc5759e2df48dc1e40b6e6
washburn <washburn@DESKTOP-PQRPMOP.localdomain>
2022-10-31 23:26:18 +0600

    refactor

--- app/src/main/java/hexlet/code/games/OddOrEvenGame.java
+++ app/src/main/java/hexlet/code/games/OddOrEvenGame.java
@@ -6,16 +6,16 @@ import main.java.hexlet.code.Utils;
 public class OddOrEvenGame {
     private static final int MAXNUMBER = 100;
     private static final int ROUNDS = 3;
-    private static final int QUESTIONARRAY = 0;
-    private static final int ANSWERARRAY = 1;
+    private static final int QUESTIONINDEX = 0;
+    private static final int ANSWERINDEX = 1;
     private static final String QUESTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
     public static void createGame() {
         String[][] roundsData = new String[ROUNDS][ROUNDS];
         for (int i = 0; i < ROUNDS; i++) {
             String[] generatedData = generateRoundData();
-            roundsData[i][0] = generatedData[QUESTIONARRAY];
-            roundsData[i][1] = generatedData[ANSWERARRAY];
+            roundsData[i][0] = generatedData[QUESTIONINDEX];
+            roundsData[i][1] = generatedData[ANSWERINDEX];
         }
         Engine.run(QUESTION, roundsData);
     }
@@ -27,8 +27,8 @@ public class OddOrEvenGame {
     public static String[] generateRoundData() {
         String[] roundData = new String[2];
         int randomNumber = Utils.generateRandom(MAXNUMBER);
-        roundData[QUESTIONARRAY] = String.valueOf(randomNumber);
-        roundData[ANSWERARRAY] = isEven(randomNumber) ? "yes" : "no";
+        roundData[QUESTIONINDEX] = String.valueOf(randomNumber);
+        roundData[ANSWERINDEX] = isEven(randomNumber) ? "yes" : "no";
         return roundData;
     }
 }
